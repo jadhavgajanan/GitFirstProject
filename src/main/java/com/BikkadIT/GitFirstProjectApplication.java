f602d82668237d579ba646eb585f64358822d6bc
jadhavgajanan <gajananjadhav1020@gmail.com>
2022-08-04 09:14:59 +0530

    commit first git project

---
+++ src/main/java/com/BikkadIT/GitFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitFirstProjectApplication.class, args);
+	}
+
+}
